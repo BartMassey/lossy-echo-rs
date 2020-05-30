54182892da5f51a6039fbdb329ca050b79074e2d
Bart Massey <bart.massey@gmail.com>
2020-05-29 18:11:32 -0700

    implemented lossiness

--- src/receive.rs
+++ src/receive.rs
@@ -1,19 +1,40 @@
 use std::ffi::CString;
 use std::net::UdpSocket;
+use std::sync::{Arc, RwLock};
 use std::{thread, time};
 
-fn main() {
-    let s = UdpSocket::bind("localhost:29001").unwrap();
-    let sleep_time = time::Duration::from_millis(100);
+fn read(s: &UdpSocket) -> String {
     let mut buf = [0; 4096];
-    loop {
-        let (nread, _where) = s.recv_from(&mut buf).unwrap();
-        if nread > buf.len() {
-            panic!("buffer overflow: {} > {}", nread, buf.len());
+    let (nread, _where) = s.recv_from(&mut buf).unwrap();
+    if nread > buf.len() {
+        panic!("buffer overflow: {} > {}", nread, buf.len());
+    }
+    let message = CString::new(&buf[..nread]).unwrap();
+    message.into_string().unwrap()
+}
+
+fn start_reader() -> Arc<RwLock<String>> {
+    let s = UdpSocket::bind("localhost:29001").unwrap();
+    let initial = read(&s);
+    let message_lock = Arc::new(RwLock::new(initial));
+    let thread_message_lock = Arc::clone(&message_lock);
+    let _ = thread::spawn(move || {
+        loop {
+            let message = read(&s);
+            let mut w = thread_message_lock.write().unwrap();
+            *w = message;
         }
-        let message = CString::new(&buf[..nread]).unwrap();
-        let message = message.into_string().unwrap();
+    });
+    message_lock
+}
+
+fn main() {
+    let message_lock = start_reader();
+    let sleep_time = time::Duration::from_millis(100);
+    loop {
+        let message = message_lock.read().unwrap();
         println!("{}", message);
+        drop(message);
         thread::sleep(sleep_time);
     }
 }
